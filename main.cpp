3ebbf819d5dcd58bcf0a652ba4cdc444d004492f
Pabloweigle <47799975+Pabloweigle@users.noreply.github.com>
2019-02-26 21:49:31 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include  <stdio.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int A = 5;
+	int B = 4;
+	
+	if (A>B)
+	{
+		printf("A eh maior que B! \n");
+	}
+	else
+	{
+		if (A<B)
+		{
+			printf("B eh maior que A! \n");
+		}
+		else
+		{
+			printf("A eh igual a B! \n");
+		}
+	}
+	
+	
+	
+	return 1;
+}
